40566658b06d6b2c8572dda6a053747d2ac898dc
root <akashshrimali1995@gmail.com>
2020-07-25 13:25:57 +0530

    NodeJS AND Angularjs CRUD complete USING AJAX

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from "@angular/forms";
 import { Router, Routes } from "@angular/router";
-import { ApiService } from  '../api.service';
 import { NotificationsService } from 'angular2-notifications';
 
 @Component({
@@ -12,7 +11,7 @@ import { NotificationsService } from 'angular2-notifications';
 
 export class BooksComponent implements OnInit {
 
-	constructor(public apiService: ApiService, public router: Router, public formBuilder: FormBuilder, public service: NotificationsService) { }
+	constructor(public router: Router, public formBuilder: FormBuilder, public service: NotificationsService) { }
 
 	addForm:FormGroup;
 	updateForm:FormGroup;
@@ -42,36 +41,5 @@ export class BooksComponent implements OnInit {
 			publisher:  new FormControl(''),
 			numOfPages:  new FormControl('')
 		});
-	}
-
-	updateBook() {
-		if(this.updateForm.status == 'INVALID'){
-		} else {
-			this.isSubmitted = true;
-			this.isbn = this.updateForm.controls.isbn.value;
-			this.title = this.updateForm.controls.title.value;
-			this.author = this.updateForm.controls.author.value;
-			this.publish_date = this.updateForm.controls.publish_date.value;
-			this.publisher = this.updateForm.controls.publisher.value;
-			this.numOfPages = this.updateForm.controls.numOfPages.value;
-			this.apiService.updateBook(this.isbn, this.title, this.author, this.publish_date, this.publisher, this.numOfPages).subscribe(
-				(res) => this.service.success('Success', res.message, {
-					position: ["top", "left"],
-					timeOut: 500,
-					animate: 'fade',
-					showProgressBar: true
-				}, this.router.navigateByUrl('/book')),
-				(err) => this.service.error('Error', err.error.message, {
-					position: ["top", "left"],
-					timeOut: 500,
-					animate: 'fade',
-					showProgressBar: true
-				}, this.router.navigateByUrl('/book'))
-			);
-		}
-	}
-
-	addBookModelClose() {
-		alert('sd');
 	}
 }
